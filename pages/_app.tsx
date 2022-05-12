5bd0611c30785bc4efe16599f17f5d3be4bf12d6
haydenlinder <haydenlinder@gmail.com>
2022-05-11 18:10:45 -0700

    Looks ok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/build.css"
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,20 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <header>Header</header>
-      <main>Content</main>
-      <footer>Footer</footer>
+      <header className="p-4 bg-black text-white mb-3 absolute w-full top-0">
+        <nav className="container">
+          Header
+        </nav>
+      </header>
+      <main className="max-h-screen min-h-screen flex justify-center overflow-y-scroll pt-28 pb-96">
+        <div className="container h-screen flex flex-col items-center">
+          <form className="flex flex-col items-center">
+            <h1>Login</h1>
+            <input className="border border-solid" placeholder="email" type="email"/>
+            <input className="border border-solid" placeholder="password" type="password"/>
+          </form>
+        </div>
+      </main>
     </>
   );
 };
