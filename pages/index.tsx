ba0787996aac405ecbbb332cd15f5d4ceec0db7f
haydenlinder <haydenlinder@gmail.com>
2022-05-15 07:07:51 -0700

    User types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { GET_USERS } from "../graphql/users";
 import client from "./api/apollo-client";
+import { GetUsersPaginatedQuery } from '../types/generated/graphql'
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const users = await client.query({ query: GET_USERS });
-  console.log({ users });
+export const getServerSideProps: GetServerSideProps<{data: GetUsersPaginatedQuery}> = async ({ req, res }) => {
+  const { data } = await client.query<GetUsersPaginatedQuery>({ query: GET_USERS });
+  data.users_connection
   return {
-    props: { users },
+    props: { data },
   };
 };
 
-const Home: NextPage = (props) => {
-  console.log({ props });
+const Home: NextPage<GetUsersPaginatedQuery> = ({ users_connection }) => {
+  console.log({ users_connection });
   return (
     <>
       <header className="p-4 bg-black text-white mb-3 absolute w-full top-0">
