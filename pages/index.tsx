71286572125f00cedc1f5e1d3ffb2f734395afb9
haydenlinder <haydenlinder@gmail.com>
2022-08-20 14:56:23 -0700

    Remove code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,16 @@
 import type { GetServerSideProps, NextPage } from "next";
 import {
-  CreatePostMutation,
-  CreatePostMutationVariables,
   GetPostsQuery,
-  GetPostsQueryVariables,
 } from "../types/generated/graphql";
-import { useMutation, useQuery, useReactiveVar } from "@apollo/client";
-import { FormEventHandler, useEffect, useState } from "react";
-import { CREATE_POST, GET_POSTS } from "../graphql/posts";
-import { currentUserIdState } from "../token";
-import { useDropzone } from "react-dropzone";
+import { GET_POSTS } from "../graphql/posts";
 import { Post } from "../components/Post";
-import { FileResponse } from "./api/images/upload";
 import { serverClient } from "./api/apollo-client";
-import { getCookieParser } from "next/dist/server/api-utils";
-
-
-async function uploadImage(file: File) {
-  const fakeImageForm = new FormData();
-  fakeImageForm.append("image", file, file.name);
-
-  const req = await fetch("/api/images/upload", {
-    method: "POST",
-    body: fakeImageForm,
-  });
-  const res: FileResponse = await req.json();
-
-  const key = res.file?.["original.webp"].key
-
-  return key;
-}
-
-interface FilePreview extends File {
-  preview: string;
-}
 
 type Props = {
   posts: GetPostsQuery['posts_connection']['edges'][0]['node'][]
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async ({ req }) => {
-  const cookies = getCookieParser(req.headers)()
-  const token = cookies.access_token
-  console.log({token})
   const { data } = await serverClient.query<GetPostsQuery>({
     query: GET_POSTS,
     context: { headers: { 'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET } }
@@ -57,35 +25,6 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({ req }) => 
 }
 
 const Home: NextPage<Props> = ({ posts }) => {
-  const [body, setBody] = useState("");
-  const [files, setFiles] = useState<FilePreview[]>([]);
-
-  const currentUserId = useReactiveVar(currentUserIdState);
-
-  
-
-  const [savePost, { loading: saving }] = useMutation<CreatePostMutation, CreatePostMutationVariables>(CREATE_POST, {
-    refetchQueries: [
-      GET_POSTS
-    ]
-  });
-
-  const handleSubmit: FormEventHandler = async (e) => {
-    e.preventDefault();
-    try {
-      const imageKeys = await Promise.all(files.map(uploadImage));
-      savePost({
-        variables: {
-          body,
-          user_id: currentUserId,
-          photo_url: imageKeys[0],
-        }
-      });
-    } catch (e) {
-      console.error({e})
-    }
-  }
-
   return (
     <section>
       <div>
@@ -95,10 +34,5 @@ const Home: NextPage<Props> = ({ posts }) => {
     </section>
   );
 };
-
-
-
-
-
 
 export default Home;
