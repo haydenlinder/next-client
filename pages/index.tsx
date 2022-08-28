1f57c770511c299cc041e81f3651fdc1c36a0942
haydenlinder <haydenlinder@gmail.com>
2022-08-28 11:49:52 -0700

    Remove serverside

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,14 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import {
   GetPostsQuery,
 } from "../types/generated/graphql";
 import { GET_POSTS } from "../graphql/posts";
-import { Post } from "../components/Post";
-import { serverClient } from "./api/apollo-client";
 import { H1 } from "../components/H1";
 import { useQuery } from "@apollo/client";
 import { Button } from "../components/Button";
 import Link from "next/link";
 
-type Props = {
-  posts: GetPostsQuery['posts_connection']['edges'][0]['node'][]
-}
-
-export const getServerSideProps: GetServerSideProps<Props> = async ({ req }) => {
-  const { data } = await serverClient.query<GetPostsQuery>({
-    query: GET_POSTS,
-    context: { headers: { 'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET } }
-  })
-  return (
-    {
-      props: {
-        posts: data.posts_connection.edges.map(e => e.node)
-      }
-    }
-  )
-}
-
-const Home: NextPage<Props> = ({ posts }) => {
+const Home: NextPage= () => {
   const { data, loading } = useQuery<GetPostsQuery>(GET_POSTS)
   const clientPosts = data?.posts_connection.edges.map(e => e.node);
   return (
