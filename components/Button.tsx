018386a93812b18cc9f3bd227fca1ab374950e97
haydenlinder <haydenlinder@gmail.com>
2022-08-28 11:46:56 -0700

    Add displayName

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,8 +2,12 @@ import React from "react"
 
 type Props = React.PropsWithChildren<React.HTMLAttributes<HTMLButtonElement>>
 
-export const Button = React.forwardRef<HTMLButtonElement, Props>(({ children, ...props }, ref) => {
+const Button = React.forwardRef<HTMLButtonElement, Props>(({ children, ...props }, ref) => {
     return (
         <button ref={ref} {...props} className={'font-bold p-2 border border-black rounded' + " " + props.className}>{children}</button>
     )
 })
+
+Button.displayName = 'Button'
+
+export {Button}
