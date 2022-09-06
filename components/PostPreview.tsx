96a1e6a90f187046fa99d80b01837ddfdc687226
haydenlinder <haydenlinder@gmail.com>
2022-09-05 20:16:47 -0700

    Remove overflow-hidden

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -53,13 +53,13 @@ export const PostPreview = ({post, user, priority, preview}: Props) => {
     )
 
     return (
-        <div className="flex flex-col w-full items-center rounded overflow-hidden min-h-fit mb-4 drop-shadow-2xl">
+        <div className="flex flex-col w-full items-center rounded min-h-fit mb-4 drop-shadow-2xl">
             {/* PHOTO */}
-            {/* <div className="p-6 z-0 h-52 w-full bg-white shadow-md">
+            <div className="p-6 z-0 h-52 rounded-t overflow-hidden w-full bg-white shadow-md">
                 <div className="relative w-full h-full">
                     {post.photo_url && <Image priority={priority} src={!preview ? `/api/images/${post.photo_url}`: post.photo_url} alt="" layout="fill" objectFit='scale-down' />}
                 </div>
-            </div> */}
+            </div>
             {/* POST INFO */}
             <div className="w-full flex min-h-[200px] justify-between items-center bg-white p-10">
                 <div>
